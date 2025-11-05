af95496b47b50181857810fdd4fd98503547ab0b
Golu Kumar <163649551+golukumarkluniversity@users.noreply.github.com>
2025-11-05 16:22:37 +0530

    Update EcommerceApplication.java

--- src/main/java/com/klu/ecommerce/EcommerceApplication.java
+++ src/main/java/com/klu/ecommerce/EcommerceApplication.java
@@ -1,18 +1,10 @@
-package com.example.k8sbeS104;
+package com.klu.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EcommerceApplication extends SpringBootServletInitializer {
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(EcommerceApplication.class);
-    }
-
+public class EcommerceApplication {
     public static void main(String[] args) {
         SpringApplication.run(EcommerceApplication.class, args);
     }
